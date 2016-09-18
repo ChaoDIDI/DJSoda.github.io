1e9bb4fa9107d755a1739a049f67b5c676e28ded
DJSoda <lixiaofeng@lixiaofengdeMacBook-Pro.local>
2016-09-18 14:52:06 +0800

    11
    11

--- 个人简历/项目展示-案例/mongodb+瀑布流+node.js/爬虫+瀑布流运用/note-app/app.js
+++ 个人简历/项目展示-案例/mongodb+瀑布流+node.js/爬虫+瀑布流运用/note-app/app.js
@@ -16,6 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/api/page/:page', (req, res) => {
   var page = req.params.page;
   page = page || 1;
